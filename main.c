d9f87eabc19600e8f35e35692fd54a8005d5a6eb
张清华 <Zqh_ithuahua@163.com>
2026-03-14 14:20:21 +0800

    提交主函数

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "menu.h"
+#include "inventory.h"
+
+
+int main() {
+
+
+
+	inventory();
+	/*switch (menu()){
+		case 1: printf("1.建立库存信息\n");break;
+		case 2: printf("2.显示库存信息\n");break;
+		case 3: printf("3.购物车\n");break;
+		case 4: printf("4.结算\n");break;
+		case 5: printf("5.退出\n");break;
+
+	}
+	*/
+
+
+	return 0;
+}
